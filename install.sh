ebac1f6cbbe3294ffd4d6cc73766ada08bc283cf
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2017-05-26 00:32:53 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,22 +7,13 @@ dotfiles=(
     .inputrc
     .nanorc
 )
-bkp_dir=~/.dotfiles_bkp
+nano_bkp_dir=~/.nano_bkp
 
 
-\mkdir -p ${bkp_dir}
+\mkdir -p ${nano_bkp_dir}
 
 for dotfile in "${dotfiles[@]}"; do
     dst_path=~/${dotfile}
-    bkp_path=${bkp_dir}/${dotfile}
 
-    # backup existing dotfiles
-    if [ -e ${dst_path} ] ; then
-        cmp -s ${dotfile} ${dst_path}
-        if [ "$?" != "0" ] ; then
-            \cp -afv ${dst_path} ${bkp_path}
-        fi
-    fi
-
-    \cp -afv ${dotfile} ${dst_path}
+    \cp -fv --backup --update ${dotfile} ${dst_path}
 done
