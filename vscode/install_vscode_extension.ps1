828903ff7b6ddd8dd167c46f64c57edda6d36d41
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-02-11 11:37:09 +0900

    Add a script to install vscode extensions

---
+++ vscode/install_vscode_extension.ps1
@@ -0,0 +1,20 @@
+# Script to install extensions for Visual Studio Code
+
+# Install golang extensions
+code --install-extension lukehoban.go
+
+# Install Python extensions
+code --install-extension freakypie.code-python-isort
+code --install-extension ms-python.python
+code --install-extension njpwerner.autodocstring
+
+# Install C++ extensions
+code --install-extension ms-vscode.cpptools
+
+# Install TypeScript extensions
+code --install-extension eg2.tslint
+
+# Install PlantUML extensions
+code --install-extension jebbs.plantuml
+
+pause
