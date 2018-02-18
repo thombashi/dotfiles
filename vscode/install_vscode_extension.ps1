b3f78becf4c6396055fb10ba66ebd9fc80d1114e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-02-18 17:22:42 +0900

    Update a vscode extension installation script

--- vscode/install_vscode_extension.ps1
+++ vscode/install_vscode_extension.ps1
@@ -1,28 +1,39 @@
 # Script to install extensions for Visual Studio Code
 
-# Install golang extensions
+
+# Install Language Extensions
+## golang
 code --install-extension lukehoban.go
 
-# Install Python extensions
+## Python
 code --install-extension freakypie.code-python-isort
 code --install-extension ms-python.python
 code --install-extension ms-vscode.powershell
 code --install-extension njpwerner.autodocstring
 
-# Install C++ extensions
+## C++
 code --install-extension ms-vscode.cpptools
 
-# Install TypeScript extensions
+## Shell Script
+code --install-extension timonwong.shellcheck
+
+## TypeScript
 code --install-extension eg2.tslint
 
-# Install docker extensions
+## etc
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension jebbs.plantuml
+
+
+# Install Application Extensions
+## docker
 code --install-extension PeterJausovec.vscode-docker
 
-# Install Jupyter Notebook extensions
-code --install-extension jithurjacob.nbpreviewer
+## Git
+code --install-extension eamodio.gitlens
 
-# Install PlantUML extensions
-code --install-extension jebbs.plantuml
+## Jupyter Notebook
+code --install-extension jithurjacob.nbpreviewer
 
 
 pause
