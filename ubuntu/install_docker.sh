6c2d21022b67e09004651dc953cb5279bc45fa8b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-03 17:16:33 +0900

    Update the docker installation script

--- ubuntu/install_docker.sh
+++ ubuntu/install_docker.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 # Reference:
-#   Get Docker CE for Ubuntu | Docker Documentation]
+#   Get Docker CE for Ubuntu | Docker Documentation
 #   https://docs.docker.com/install/linux/docker-ce/ubuntu/
+#   (amd64)
 
 if [ $UID -ne 0 ]; then
     echo 'requires superuser privilege' 1>&2
     exit 13
 fi
+
+
+# Uninstall older versions of Docker
+sudo apt remove docker docker-engine docker.io
 
 # Install packages to allow apt to use a repository over HTTPS:
 https_packages=(
@@ -33,3 +38,12 @@ apt update
 
 # Install Docker CE
 apt install docker-ce
+
+
+# Reference:
+#   Post-installation steps for Linux | Docker Documentation
+#   https://docs.docker.com/install/linux/linux-postinstall/
+
+# Manage Docker as a non-root user
+groupadd docker
+gpasswd -a $USER docker
