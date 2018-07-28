20d7bb70ceb55608922a199c8e498535a5b4308d
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-07-28 16:14:58 +0900

    Update packages

--- windows/install_package_.ps1
+++ windows/install_package_.ps1
@@ -1,2 +1,3 @@
 # Script to install Windows packages via chocolatey
 Start-Process choco -Verb runas -Wait -ArgumentList "install packages.config -y"
+Start-Process choco -Verb runas -Wait -ArgumentList "pin add -n=python"
