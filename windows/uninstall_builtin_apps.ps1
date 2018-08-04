5b6297b8db9edaa8fb0c45494ddfb22808ea8e7c
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-08-04 10:22:27 +0900

    Update packages

--- windows/uninstall_builtin_apps.ps1
+++ windows/uninstall_builtin_apps.ps1
@@ -1,17 +1,22 @@
-Get-AppxPackage *3d* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.3DViewer* | Remove-AppxPackage
-Get-AppxPackage *messaging* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.MSPaint* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.People* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Print3D* | Remove-AppxPackage
-Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.XboxApp* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage
-
-Get-AppxPackage *.Twitter | Remove-AppxPackage
-Get-AppxPackage Flipboard.Flipboard | Remove-AppxPackage
+Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
 Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
 Get-AppxPackage Microsoft.FreshPaint | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage Microsoft.MSPaint* | Remove-AppxPackage
+Get-AppxPackage Microsoft.NetworkSpeedTest | Remove-AppxPackage
+Get-AppxPackage Microsoft.People* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Print3D* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.Photos
+Get-AppxPackage Microsoft.WindowsMaps* | Remove-AppxPackage
 Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
-Get-AppxPackage D5EA27B7.Duolingo-LearnLanguagesforFree | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox* | Remove-AppxPackage
+Get-AppxPackage Microsoft.ZuneMusic* | Remove-AppxPackage
+
+Get-AppxPackage *.AdobePhotoshopExpress | Remove-AppxPackage
+Get-AppxPackage *.Duolingo-LearnLanguagesforFree | Remove-AppxPackage
+Get-AppxPackage *.Twitter | Remove-AppxPackage
+Get-AppxPackage *messaging* | Remove-AppxPackage
+Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage Flipboard.Flipboard | Remove-AppxPackage
