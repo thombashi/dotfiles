d5cc2dacae6caf58d33e3e544d6d90ae76de7c0a
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-02 15:30:09 +0900

    Remove fish

--- ubuntu/install_package.sh
+++ ubuntu/install_package.sh
@@ -40,11 +40,7 @@ packages=(
 )
 optional_packages=(
     cscope
-    fish
 )
-
-# Add fish nightly builds repository: https://github.com/fish-shell/fish-shell/wiki/Nightly-builds
-#echo 'deb http://download.opensuse.org/repositories/shells:/fish:/nightly:/master/Debian_8.0/ /' > /etc/apt/sources.list.d/fish.list
 
 apt update
 apt -y install "${packages[@]}"
