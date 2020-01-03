8bbca6562c1e0b251324f427f9b192c0034995bb
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 00:45:50 +0900

    Update npm installation

--- ubuntu/install_package.sh
+++ ubuntu/install_package.sh
@@ -33,7 +33,6 @@ packages=(
     lazygit
     moreutils
     nmap
-    npm
     openjdk-8-jdk-headless
     pandoc
     peco
@@ -55,3 +54,14 @@ optional_packages=(
 apt update
 apt -y install --no-install-recommends "${packages[@]}"
 #apt -y install "${optional_packages[@]}"
+
+snap install micro --classic
+snap install yq
+
+# install the latest lts npm
+if ! n lts --version > /dev/null 2>&1; then
+    apt -y install --no-install-recommends npm
+    npm install -g n
+    n lts
+    apt purge nodejs npm -y --autoremove
+fi
