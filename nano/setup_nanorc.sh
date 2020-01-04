b974b5167252585bbeebf8142f4b47ec3a18defe
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 15:33:02 +0900

    Update setup_nanorc.sh

--- nano/setup_nanorc.sh
+++ nano/setup_nanorc.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# make a directory for backup
+\mkdir -p "${HOME}/.nano_bkp"
+
 \cp -fva --backup --update .nanorc "${HOME}/.nanorc"
 
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
