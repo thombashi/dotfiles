c7b978b4f4b99bac24e39a2cc2d6843e2a6d7bfa
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-08-14 19:09:15 +0900

    Update install_go.sh

--- ubuntu/install_go.sh
+++ ubuntu/install_go.sh
@@ -17,11 +17,16 @@ INSTALL_ROOT="/usr/local"
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 MACHINE=$(uname -m)
 
+if ! echo $GO_VERSION | \grep -P "\d+(\.\d+){2,}"; then
+  echo "invalid go version: ${GO_VERSION}" 1>&2
+  exit 1
+fi
+
 if [ "$MACHINE" = "x86_64" ]; then
   MACHINE="amd64"
 fi
 
 rm -rf "${INSTALL_ROOT}/go"
-echo "installing golang ${GO_VERSION} ..."
+echo "installing golang ${GO_VERSION} to ${INSTALL_ROOT}/go ..."
 curl -fsL https://go.dev/dl/go${GO_VERSION}.${OS}-${MACHINE}.tar.gz | tar xzf - -C ${INSTALL_ROOT}
 ${INSTALL_ROOT}/go/bin/go version
